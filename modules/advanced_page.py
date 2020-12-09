a4070520067d69df119d8c14048c47ad8701e352
Marisa Smith <mesmith027@gmail.com>
2020-12-09 18:06:18 -0500

    noted functions to be added to progress & status

--- modules/advanced_page.py
+++ modules/advanced_page.py
@@ -6,7 +6,9 @@ def advanced(page):
     '''Advanced Streamlit commands'''
     st.title(page)
 
-
+    st.subheader('Progress and status') 
+    # st.progress, st.spinner, error, balloons, warning, info, success, exception 
+    
     #with st.spinner('working on it'): 
     #    time.sleep(5)
     #st.success('done!')

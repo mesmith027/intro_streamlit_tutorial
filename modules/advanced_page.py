8ee890d76948194b4fc8d7494d6913205ffa8a1e
Marisa Smith <mesmith027@gmail.com>
2020-12-11 19:08:39 -0500

    added error and warning

--- modules/advanced_page.py
+++ modules/advanced_page.py
@@ -16,10 +16,10 @@ def advanced(page):
     st.header('Progress and Status') 
     st.write("""These commands can be very useful for managing a users expectation if you are loading a large data set, running 
     an expensive computation or just wanting to keep your app user updates on whats happening behind the scenes! :eyes: 
-    Lets see what cool things you can do to keep your user occupied while your app is updating
+    Lets see what cool things you can do to keep your user occupied while your app is updating.
     """)
     col1, mid, col2 = st.beta_columns([20,0.5,20])
-    # st.progress, st.spinner, error, balloons, warning, info, success, exception 
+    #  balloons, warning, info, success, exception 
     
     with col1:
         st.subheader('Spinner')
@@ -34,12 +34,30 @@ with st.spinner('working on it'):
             with st.spinner('working on it'): 
                 time.sleep(3)
                 st.write("done!")
-                #st.success('done!')
+        
+        st.markdown("---")
+
+        st.subheader("Error")
+        st.write("Use this to let your user know something went wrong, without breaking your app!")
+        st.code("""
+text = st.text_input("Type your name here")
+if len(text) == 0: 
+    st.error('You must enter your name to continue')
+else: 
+    st.write("Hey %s :wave:" %text)
+        """)
+
+        text = st.text_input("Type your name here")
+
+        if len(text) == 0: 
+            st.error('You must enter your name to continue')
+        else: 
+            st.write("Hey %s :wave:" %text)
         st.markdown("---")
 
     with col2: 
         st.subheader("Success")
-        st.write("use this when you want to let your user know that a task as been successful")
+        st.write("Use this when you want to let your user know that a task as been successful")
         st.code("""
 column_names = ['a', 'b']
 data = pd.DataFrame(np.random.randn(10,2), columns=column_names)
@@ -51,6 +69,30 @@ st.success("Created Pandas DataFrame")
             data = pd.DataFrame(np.random.randn(10,2), columns=column_names)
             st.success("Created Pandas DataFrame")
         st.markdown("---")
+
+        st.subheader("Warning")
+        st.write("Use this to let your user know something could go wrong")
+        st.code("""
+check = st.checkbox("Run this code")
+if check: 
+    st.info("Something may happen that I can't control....")
+    n = np.random.normal()
+    if n < 0: 
+        st.write(n)
+    else: 
+        st.write('Yup something happened!')
+        """)
+
+        check = st.checkbox("Run this code", key="info_run")
+        if check: 
+            st.info("Something may happen that I can't control....")
+            n = np.random.normal()
+            if n < 0: 
+                st.write(n)
+            else: 
+                st.write('Yup something happened!')
+        st.markdown("---")
+
     # st.cache 
     # st.stop()
     return

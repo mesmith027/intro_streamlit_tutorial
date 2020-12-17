747a056da4ba6bf75dcf22dbb62cdce94e287c29
Marisa Smith <mesmith027@gmail.com>
2020-12-17 16:54:31 -0500

    made sidebar into a module

--- expansion_app.py
+++ expansion_app.py
@@ -5,6 +5,7 @@ from modules.basic_page import basic
 from modules.layout_page import layout 
 from modules.advanced_page import advanced
 from modules.tutorial_page import tutorial
+from modules.my_sidebar import sidebar
 
 # Run the program
 if __name__ == '__main__':
@@ -12,20 +13,8 @@ if __name__ == '__main__':
     # Create a wide layout for the pages
     st.set_page_config(page_title='Streamlit cheat sheet',layout="wide")
 
-    # Create a sidebar with all the page options for the expansion
-    st.sidebar.markdown('Pick the page you wish to visit for a list of all the \
-    possible functions and commands in Streamlit')
-    page = st.sidebar.radio('Navigation', [
-        'Information and Command line',
-        'Basic Commands',
-        'Layout Commands',
-        'Advanced Commands',
-        'Try it Yourself!'])
-
-    # in sidebar add links to the documentation and version this is based on
-    st.sidebar.markdown('''
-<small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v0.71.0](https://www.streamlit.io/).</small>
-    ''', unsafe_allow_html=True)
+    # create the sidebar
+    page = sidebar()
 
     # run associated page program for each selection
     if page == 'Information and Command line':
---
+++ modules/my_sidebar.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+def sidebar():
+    
+    #add streamlit logo
+    st.sidebar.image("img/row_1_col_0.png", width=300)
+
+    # Create a sidebar with all the page options for the expansion
+    st.sidebar.markdown('Pick the page you wish to visit for a list of all the \
+    possible functions and commands in Streamlit')
+    page = st.sidebar.radio('Navigation', [
+        'Information and Command line',
+        'Basic Commands',
+        'Layout Commands',
+        'Advanced Commands',
+        'Try it Yourself!'])
+
+    # in sidebar add links to the documentation and version this is based on
+    st.sidebar.markdown('''
+<small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v0.71.0](https://www.streamlit.io/).</small>
+    ''', unsafe_allow_html=True)
+    return page

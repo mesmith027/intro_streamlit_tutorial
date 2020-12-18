e5e9be195f6e773642ecae685e13521f5897bf4a
Marisa Smith <mesmith027@gmail.com>
2020-12-18 12:28:21 -0500

    moved logo below sidebar

--- modules/my_sidebar.py
+++ modules/my_sidebar.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 def sidebar():
     
-    #add streamlit logo
-    st.sidebar.image("img/row_1_col_0.png", width=300)
-
     # Create a sidebar with all the page options for the expansion
     st.sidebar.markdown('Pick the page you wish to visit for a list of all the \
     possible functions and commands in Streamlit')
@@ -19,4 +16,10 @@ def sidebar():
     st.sidebar.markdown('''
 <small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v0.71.0](https://www.streamlit.io/).</small>
     ''', unsafe_allow_html=True)
+
+    #add streamlit logo
+    st.sidebar.image("img/row_1_col_0.png", width=100, )
+
+
+    # return the page choice made by the user
     return page
